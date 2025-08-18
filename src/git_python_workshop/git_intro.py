bc8da4fb51346ab2c263dd04ad2e48eb6ec91511
Eliot Kim <ejkim@Eliots-MacBook-Pro-3.local>
2025-08-18 16:40:13 -0700

    Barebones git intro file.

--- src/git_python_workshop/git_intro.py
+++ src/git_python_workshop/git_intro.py
@@ -1,25 +1,82 @@
-import numpy as np
-
-def add():
+def add(a: int, b: int):
     """
-    Returns the sum of two numbers.
+    Add two integers.
+
+    Parameters
+    ----------
+    a : int
+        The first integer.
+    b : int
+        The second integer.
+
+    Returns
+    -------
+    int
+        The sum of a and b.
     """
     return -1
 
-def subtract():
+def subtract(a: int, b: int):
     """
-    Returns the difference between two numbers.
+    Subtract one integer from another.
+
+    Parameters
+    ----------
+    a : int
+        The integer to subtract from.
+    b : int
+        The integer to subtract.
+
+    Returns
+    -------
+    int
+        The difference between a and b.
     """
     return -1
 
-def multiply():
+def multiply(a: int, b: int):
     """
-    Returns the product of two numbers.
+    Multiply two integers.
+
+    Parameters
+    ----------
+    a : int
+        The first integer.
+    b : int
+        The second integer.
+
+    Returns
+    -------
+    int
+        The product of a and b.
     """
     return -1
 
-def divide():
+def divide(a: int, b: int):
     """
-    Returns the quotient of dividing two numbers.
+    Divide one integer by another.
+
+    Parameters
+    ----------
+    a : int
+        The numerator.
+    b : int
+        The denominator.
+
+    Returns
+    -------
+    int
+        The quotient of a divided by b.
     """
     return -1
+
+if __name__ == "__main__":
+
+    start_num = 1
+
+    temp_num = add(1, 2)
+    temp_num = subtract(temp_num, 1)
+    temp_num = multiply(temp_num, 6)
+    temp_num = divide(temp_num, 3)
+
+    print(f"Final number: {temp_num}")
