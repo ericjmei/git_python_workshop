ed1274c9cf23bc054dc61708be3673c4d58b2bab
Eliot Kim <ejkim@Eliots-MacBook-Pro-3.local>
2025-08-18 14:35:16 -0700

    Started git intro file.

---
+++ src/git_python_workshop/git_intro.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def add():
+    """
+    Returns the sum of two numbers.
+    """
+    return -1
+
+def subtract():
+    """
+    Returns the difference between two numbers.
+    """
+    return -1
+
+def multiply():
+    """
+    Returns the product of two numbers.
+    """
+    return -1
+
+def divide():
+    """
+    Returns the quotient of dividing two numbers.
+    """
+    return -1
