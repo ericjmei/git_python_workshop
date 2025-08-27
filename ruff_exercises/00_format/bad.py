01d061825a2b817627f4c78efdf8beac570d8099
Eric <eric.mei2000@gmail.com>
2025-08-27 09:20:42 -0700

    exercise 00 (ruff format)

--- ruff_exercises/00_format/bad.py
+++ ruff_exercises/00_format/bad.py
@@ -5,4 +5,4 @@ def foo (x:int=  1, y =2):
         if(x> 10):   return  x* y
         return x+  y
 
-long_str = "this is a really long string that probably should wrap or get split to respect line length limits and trailing whitespace"     
+long_str = "this is a really long string that probably should wrap or get split to respect line length limits and trailing whitespace"          
---
+++ ruff_exercises/00_format/good.py
@@ -0,0 +1,8 @@
+def foo(x: int = 1, y=2):
+    """does stuff"""
+    if x > 10:
+        return x * y
+    return x + y
+
+
+long_str = "this is a really long string that probably should wrap or get split to respect line length limits and trailing whitespace" # why didn't this get fixed?
