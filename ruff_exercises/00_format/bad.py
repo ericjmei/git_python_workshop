5a2cfe1d9b8dd0a8945f9807cb962337c34ac749
Eric <eric.mei2000@gmail.com>
2025-08-27 09:24:38 -0700

    added another exercise to ruff format

--- ruff_exercises/00_format/bad.py
+++ ruff_exercises/00_format/bad.py
@@ -6,3 +6,5 @@ def foo (x:int=  1, y =2):
         return x+  y
 
 long_str = "this is a really long string that probably should wrap or get split to respect line length limits and trailing whitespace"          
+
+long_dict = {"key_0": "value_0", "key_1": "value_1", "key_2": "value_2", "key_3": "value_3", "key_4": "value_4"}
